05c496bc3f2acb1024cea57437a48d4d01aad971
Xiao-Chenguang <973990275@qq.com>
2023-04-05 11:22:31 +0100

    add echo and fix wrong command

--- run_svm.sh
+++ run_svm.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 # change to the directory of this script
+echo "Changing to the directory of this script"
 cd $(dirname $0)
 
 # download the data
+echo "Downloading the data"
 python src/cl_download_data.py
 
 # convert the data to csv file
-python src/cl_convert_data.py
+echo "Converting the data to csv file"
+python src/cl_to_binary_csv.py
 
 # run the cnetralized learning
+echo "Running the centralized learning"
 python run_svm.py
