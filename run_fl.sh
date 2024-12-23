aef6be27118d082d5c1a73267984653485d68893
Xiao-Chenguang <973990275@qq.com>
2024-12-23 15:44:34 +0000

    makedir dirs if not exist

--- run_fl.sh
+++ run_fl.sh
@@ -6,6 +6,8 @@ cd $(dirname $0)
 
 # create log directory if not exists
 mkdir -p log
+mkdir -p datasets/femnist
+mkdir -p results
 
 # unzip the femnist dataset if not exists
 if [ ! -f ./datasets/femnist/write_digits.hdf5 ]; then
